4b6a94a75469379456b06e34a1375f57f7db6e89
Daiki Horiyama <d19921207@gmail.com>
2019-12-27 03:02:11 +0900

    タイムトラベルの日付のケースを増やした

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -26,8 +26,7 @@ class HomeTest extends TestCase
     public function home画面で現在時刻が表示される(
         Carbon $now,
         string $messageExpected
-    )
-    {
+    ) {
         // ----------------------------------------
         // 1. setup
         // ----------------------------------------
@@ -53,13 +52,14 @@ class HomeTest extends TestCase
      * @dataProvider dataProvider_timetravelパラメータと表示
      */
     public function timetravelパラメータが指定されているとその日時が表示される(
+        Carbon $now,
         string $timetravel,
         string $messageExpected
-    )
-    {
+    ) {
         // ----------------------------------------
         // 1. setup
         // ----------------------------------------
+        Carbon::setTestNow($now);
         $this->be(User::first());
 
         // ----------------------------------------
@@ -84,16 +84,37 @@ class HomeTest extends TestCase
     public function dataProvider_現在時刻と表示(): iterable
     {
         yield [
-            Carbon::create(2019,12,31,23,59,59),
+            Carbon::create(2019, 12, 31, 23, 59, 59),
             '2019-12-31 23:59:59',
         ];
     }
 
     public function dataProvider_timetravelパラメータと表示(): iterable
     {
-        yield [
+        yield '昨日' => [
+            Carbon::create(2020, 1, 1, 12, 34, 56),
             '2019-12-31',
             '2019-12-31 00:00:00',
+        ];
+        yield '1年前' => [
+            Carbon::create(2020, 1, 1, 12, 34, 56),
+            '2019-01-01',
+            '2019-01-01 00:00:00',
+        ];
+        yield '明日' => [
+            Carbon::create(2020, 1, 1, 12, 34, 56),
+            '2020-01-02',
+            '2020-01-02 00:00:00',
+        ];
+        yield '来週' => [
+            Carbon::create(2020, 1, 1, 12, 34, 56),
+            '2020-01-08',
+            '2020-01-08 00:00:00',
+        ];
+        yield '来年' => [
+            Carbon::create(2020, 1, 1, 12, 34, 56),
+            '2021-01-01',
+            '2021-01-01 00:00:00',
         ];
     }
 }
